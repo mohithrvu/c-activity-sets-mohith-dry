3261cbac2a2caee9ee249d7dd6bc66158be81a63
mohithrvu <119392357+mohithrvu@users.noreply.github.com>
2022-12-08 09:01:11 +0530

    Update problem07.c

---
+++ set01/problem07.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  a=input();
+  b=nat_sum(a);
+  output(a,b);
+}
+int input()
+{
+  int n;
+  printf("enter a number");
+  scanf("%d", &n);
+  return n;
+}
+int nat_sum(int n,int sum)
+{
+  sum=(n*(n+1))/2;
+  return sum;
+}
+void output(int a,int b){
+  printf("the sum of natural numbers %d is %d",a,b);
+}
